867cf0c0374f16afca27349a3f0614f65958f8c9
manavld <mlakhwani97@gmail.com>
2020-12-16 21:38:40 +0100

    video

---
+++ Dades/Assets/EventData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventData 
+{
+    uint EventID;
+
+    public string GetJSON()
+    {
+        string json = JsonUtility.ToJson(this);
+        return json;
+    }
+    
+}
+
+public class KillEvent : EventData
+{
+    Vector3 Position;
+    Vector3 EulerAngles;
+   
+}
+
+public class RegisterEvent : EventData
+{
+    string UserName;
+
+} 
