5fdeee5a05a2c15efb99e0e2ae07ba438b7846c8
manavld <mlakhwani97@gmail.com>
2020-12-16 19:52:09 +0100

    ReImport

--- Dades/Assets/3DGamekitLite/Scripts/Editor/NewSceneCreator.cs
+++ Dades/Assets/3DGamekitLite/Scripts/Editor/NewSceneCreator.cs
@@ -12,55 +12,54 @@ namespace Gamekit3D
     {
         protected string m_NewSceneName;
 
-        protected readonly GUIContent m_NameContent = new GUIContent ("New Scene Name");
-    
+        protected readonly GUIContent m_NameContent = new GUIContent("New Scene Name");
+
         [MenuItem("Kit Tools/Create New Scene...", priority = 100)]
-        static void Init ()
+        static void Init()
         {
-            NewSceneCreator window = GetWindow<NewSceneCreator> ();
+            NewSceneCreator window = GetWindow<NewSceneCreator>();
             window.Show();
         }
 
-        void OnGUI ()
+        void OnGUI()
         {
-            m_NewSceneName = EditorGUILayout.TextField (m_NameContent, m_NewSceneName);
-        
-            if(GUILayout.Button ("Create"))
-                CheckAndCreateScene ();
+            m_NewSceneName = EditorGUILayout.TextField(m_NameContent, m_NewSceneName);
+
+            if (GUILayout.Button("Create"))
+                CheckAndCreateScene();
         }
 
         protected void CheckAndCreateScene()
         {
             if (EditorApplication.isPlaying)
             {
-                Debug.LogWarning ("Cannot create scenes while in play mode.  Exit play mode first.");
+                Debug.LogWarning("Cannot create scenes while in play mode.  Exit play mode first.");
                 return;
             }
 
-            Scene currentActiveScene = SceneManager.GetActiveScene ();
+            Scene currentActiveScene = SceneManager.GetActiveScene();
 
             if (currentActiveScene.isDirty)
             {
                 string title = currentActiveScene.name + " Has Been Modified";
                 string message = "Do you want to save the changes you made to " + currentActiveScene.path + "?\nChanges will be lost if you don't save them.";
-                int option = EditorUtility.DisplayDialogComplex (title, message, "Save", "Don't Save", "Cancel");
+                int option = EditorUtility.DisplayDialogComplex(title, message, "Save", "Don't Save", "Cancel");
 
                 if (option == 0)
                 {
-                    EditorSceneManager.SaveScene (currentActiveScene);
+                    EditorSceneManager.SaveScene(currentActiveScene);
                 }
                 else if (option == 2)
                 {
                     return;
                 }
             }
-        
-            CreateScene ();
 
-            GetWindow<ProBuilder.EditorCore.pb_Editor>();
+            CreateScene();
+
         }
 
-        protected void CreateScene ()
+        protected void CreateScene()
         {
             string[] result = AssetDatabase.FindAssets("_TemplateScene");
 
@@ -82,10 +81,10 @@ namespace Gamekit3D
             }
         }
 
-        protected void AddSceneToBuildSettings (Scene scene)
+        protected void AddSceneToBuildSettings(Scene scene)
         {
             EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes;
-        
+
             EditorBuildSettingsScene[] newBuildScenes = new EditorBuildSettingsScene[buildScenes.Length + 1];
             for (int i = 0; i < buildScenes.Length; i++)
             {
@@ -95,21 +94,21 @@ namespace Gamekit3D
             EditorBuildSettings.scenes = newBuildScenes;
         }
 
-        protected GameObject InstantiatePrefab (string folderPath, string prefabName)
+        protected GameObject InstantiatePrefab(string folderPath, string prefabName)
         {
             GameObject instance = null;
             string[] prefabFolderPath = { folderPath };
-            string[] guids = AssetDatabase.FindAssets (prefabName, prefabFolderPath);
-        
-            if(guids.Length == 0)
-                Debug.LogError ("The " + prefabName + " prefab could not be found in " + folderPath + " and could therefore not be instantiated.  Please create one manually.");
-            else if(guids.Length > 1)
-                Debug.LogError ("Multiple " + prefabName + " prefabs were found in " + folderPath + " and one could therefore not be instantiated.  Please create one manually.");
+            string[] guids = AssetDatabase.FindAssets(prefabName, prefabFolderPath);
+
+            if (guids.Length == 0)
+                Debug.LogError("The " + prefabName + " prefab could not be found in " + folderPath + " and could therefore not be instantiated.  Please create one manually.");
+            else if (guids.Length > 1)
+                Debug.LogError("Multiple " + prefabName + " prefabs were found in " + folderPath + " and one could therefore not be instantiated.  Please create one manually.");
             else
             {
-                string path = AssetDatabase.GUIDToAssetPath (guids[0]);
-                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject> (path);
-                instance = Instantiate (prefab);
+                string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                instance = Instantiate(prefab);
             }
 
             return instance;
